bc8f8f33311ce3f6d6bcb4283951f4bd0af334ae
Tivon Naicker <naicker.tivon@gmail.com>
2024-12-12 16:57:31 +0200

    Deleted revealdata.cs

--- AKKTN Pr00/Models/revealData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AKKTN_Pr00.Models
-{
-    public class revealData
-    {
-        public string CompanyID {  get; set; }
-        public string ClientID { get; set; }
-        public string pass {  get; set; }
-    }
-}
