46f5bd63f4b807c4adfded1bb2bfc71c37bbfd27
Tivon Naicker <139761611+TivonNaicker@users.noreply.github.com>
2025-01-06 17:00:36 +0200

    Delete Login and Registration directory

--- Login and Registration/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Login_and_Registration.Models
-{
-    public class User : IdentityUser
-    {
-        public string FullName { get; set; }
-    }
-}
