d16409a96047dc18a59956d4b65a8a1b8dc0bf80
Ngcebo Langa <20230799@ctucareer.co.za>
2025-01-14 18:50:03 +0200

    update again forms#2

--- AKKTN Pr00/Program.cs
+++ AKKTN Pr00/Program.cs
@@ -55,6 +55,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Tasks}/{action=Index}/{id?}");
 
 app.Run();
