f2bb8850f100f6be01a6e2b7b7ba6e5d95620847
Tivon Naicker <139761611+TivonNaicker@users.noreply.github.com>
2025-01-28 21:36:00 +0200

    Delete Login and Registration directory

--- Login and Registration/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Login_and_Registration.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
